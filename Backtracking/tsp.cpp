6c54272de0cfba27e8b7370a1fa973c9f9a84c7b
Duong <duong19@github.com>
2020-04-17 12:44:26 +0700

    Add dynamic programming ex

--- Backtracking/tsp.cpp
+++ Backtracking/tsp.cpp
@@ -1,19 +1,15 @@
-#include <iostream>
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
+int optCost[1<<20][20];
 int cost[50][50];
-int optCost[1 << 20][20];
-
-
-
 
 int min_cost(int a, int b)
 {
-	if (a == -1) return b;
-	if (a < b) return b;
-	return a;
+	if (a < b) return a;
+	return b;
 }
 
 int TSP(int n, int k, int bM, int lastC){
@@ -22,6 +18,7 @@ int TSP(int n, int k, int bM, int lastC){
 	{
 		return optCost[bM][lastC] = cost[lastC][1];
 	}
+	optCost[bM][lastC] = 1e9;
 	for (int i = 2; i <= n; i ++)
 	{
 		if (((bM >> i-1) & 1) == 0)
@@ -29,6 +26,7 @@ int TSP(int n, int k, int bM, int lastC){
 			optCost[bM][lastC] = min_cost(optCost[bM][lastC], cost[lastC][i] + TSP(n, k+1, bM | (1 << i-1), i));
 		}  
 	}
+	return optCost[bM][lastC];
 	
 }
 
@@ -47,8 +45,7 @@ int main()
 		{
 			optCost[i][j] = -1;
 		}
-	TSP(n, 1, 1, 1);
-	cout << optCost[1][1];
+	cout << TSP(n, 1, 1, 1);
 	return 0;	 
 }
 
